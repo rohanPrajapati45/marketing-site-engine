4e3b9bc15123c10ee983e0d5cce49544a6e1f622
mann-prajapati05 <mannprajapati0284@gmail.com>
2026-05-11 10:41:27 +0530

    branch testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <div>
-      <h1 className='bg-blue-600'>Hello, Vite + React!</h1>
+      <h1 className="bg-blue-600">Hello, Vite + React!</h1>
+      <h2>okkkk</h2>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
