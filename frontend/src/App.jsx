327e0f6253bc0b6f3b7731f772541bd431afa65e
rohanPrajapati45 <rohanjp211@gmail.com>
2026-05-11 12:34:51 +0530

    footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="bg-blue-600">Hello, Vite + React!</h1>
-      <h2>okkkk</h2>
-      <h3>Navbar</h3>
-      <p>change in nav branch - done....</p>
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-grow">
+        <h1 className="bg-blue-600">Hello, Vite + React!</h1>
+        <h2>okkkk</h2>
+        <h3>Navbar</h3>
+        <p>change in nav branch - done....</p>
+      </main>
+      <Footer />
     </div>
   );
 };
