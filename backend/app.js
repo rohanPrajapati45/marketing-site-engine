cc105a5377d500d32cf296b3d7acd673c5b0adae
mann-prajapati05 <mannprajapati0284@gmail.com>
2026-05-11 10:30:58 +0530

    init backend

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+const app=express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/',(req,res,next)=>{
+    console.log(req.method , req.url);
+    next();
+})
+
+const PORT=process.env.PORT;
+
+const startServer = async () => {
+    try {
+        app.listen(PORT,()=>{
+            console.log(`Server is running on http://localhost:${PORT}/`);
+        });
+    } catch (err) {
+        console.log("Failed to start server due to DB connection error..", err);
+        process.exit(1);
+    }
+};
+
+startServer();
