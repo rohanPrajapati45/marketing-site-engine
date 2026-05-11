9c3193e13f825db38c44047405ed52bdec4a687c
mann-prajapati05 <mannprajapati0284@gmail.com>
2026-05-11 13:02:41 +0530

    fix sizing

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -3,47 +3,44 @@ import React from "react";
 function Nav() {
   return (
     <div>
-      <div
-        className="flex justify-between items-center px-8 py-6 bg-white text-black"
-        mt-8
-      >
+      <div className="flex justify-between items-center px-8 py-6 bg-transparent text-black mt-7">
         <div>
           <a href="#">
             <img
-              src="/logo/logo-black.svg"
+              src="./src/assets/logo/tedmob_logo_dark.svg"
               alt="Tedmob Logo"
-              className="h-8 w-auto object-contain"
+              className="h-5 w-auto object-contain"
             />
           </a>
         </div>
-        <ul className="flex gap-10 shrink-0 list-none">
+        <ul className="flex gap-12 shrink-0 list-none">
           <li>
-            <a href="" className="no-underline font-medium text-sm">
+            <a href="" className="no-underline font-medium text-[1rem]">
               WORK
             </a>
           </li>
           <li>
-            <a href="" className="no-underline font-medium text-sm">
+            <a href="" className="no-underline font-medium text-[1rem]">
               AGENCY
             </a>
           </li>
           <li>
-            <a href="" className="no-underline font-medium text-sm">
+            <a href="" className="no-underline font-medium text-[1rem]">
               WHAT WE DO
             </a>
           </li>
           <li>
-            <a href="" className="no-underline font-medium text-sm">
+            <a href="" className="no-underline font-medium text-[1rem]">
               SOLUTIONS
             </a>
           </li>
           <li>
-            <a href="" className="no-underline font-medium text-sm">
+            <a href="" className="no-underline font-medium text-[1rem]">
               BLOG
             </a>
           </li>
           <li>
-            <a href="" className="no-underline font-medium text-sm">
+            <a href="" className="no-underline font-medium text-[1rem]">
               CONTACT
             </a>
           </li>
