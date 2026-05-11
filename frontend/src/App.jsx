ad15aa865062e68679978bb98df3de09b69d1b72
mann-prajapati05 <mannprajapati0284@gmail.com>
2026-05-11 10:57:46 +0530

    chnage in nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
       <h1 className="bg-blue-600">Hello, Vite + React!</h1>
       <h2>okkkk</h2>
       <h3>Navbar</h3>
-      <p>change in nav branch</p>
+      <p>change in nav branch - done....</p>
     </div>
   );
 };
