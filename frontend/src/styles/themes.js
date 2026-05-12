f0922877a2480ccde0100c56f993ac45a9082ee9
mann-prajapati05 <mannprajapati0284@gmail.com>
2026-05-12 18:18:35 +0530

    nav theme update

--- frontend/src/styles/themes.js
+++ frontend/src/styles/themes.js
@@ -4,6 +4,26 @@ export const pageThemes = {
     bg: "bg-transparent",
     navbar: "bg-transparent text-white",
   },
+  "/work": {
+    bg: "bg-transparent",
+    navbar: "bg-white text-black",
+  },
+  "/what-we-do": {
+    bg: "bg-transparent",
+    navbar: "bg-white text-black",
+  },
+  "/solutions": {
+    bg: "bg-transparent",
+    navbar: "bg-white text-black",
+  },
+  "/blog": {
+    bg: "bg-transparent",
+    navbar: "bg-white text-black",
+  },
+  "/contact": {
+    bg: "bg-transparent",
+    navbar: "bg-transparent text-white",
+  },
   "/agency": {
     bg: `
     bg-[url('https://tedmob-cop1-files.s3.amazonaws.com/tedmob.com/storage/website-sections/2028670989.webp')]
