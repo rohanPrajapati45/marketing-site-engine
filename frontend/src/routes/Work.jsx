2fe334f8d2e611884aca33eed2cc946337d58f05
Ayush Prajapati <ayushprajapati15806@gmail.com>
2026-05-12 16:01:41 +0530

    init Work_page

--- frontend/src/routes/Work.jsx
+++ frontend/src/routes/Work.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import WorkHero from "../components/WorkHero";
-import WorkCategoryMenu from "../components/WorkCategoryMenu";
+import WorkCategoryMenu from "../components/Work_page/WorkCategoryMenu";
 // import RecentProjectsSection from "../components/RecentProjectsSection";
 
 function Work() {
