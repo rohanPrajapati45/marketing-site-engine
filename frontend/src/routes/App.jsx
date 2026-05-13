8096b8877832da4c388f008d7407c95a4d56349c
rohanPrajapati45 <rohanjp211@gmail.com>
2026-05-13 18:40:19 +0530

    contact page

--- frontend/src/routes/App.jsx
+++ frontend/src/routes/App.jsx
@@ -9,14 +9,11 @@ import { pageThemes } from "../styles/themes";
 const App = () => {
   const location = useLocation();
   const isHome = location.pathname === "/";
-<<<<<<< HEAD
   const isContact = location.pathname === "/contact";
   const isNavOverlay = isHome || isContact;
-=======
   const [showSplash, setShowSplash] = useState(false);
   const [splashKey, setSplashKey] = useState(0);
   const [hideNav, setHideNav] = useState(false);
->>>>>>> 7cb861251cf7e6c992449746c7b2e5ac6b2dceb4
 
   const theme = pageThemes[location.pathname] || pageThemes["/"];
 
@@ -34,13 +31,8 @@ const App = () => {
 
   return (
     <div className={`relative min-h-screen flex flex-col ${theme.bg}`}>
-<<<<<<< HEAD
-      {showSplash && <SplashScreen />}
-      <Nav replaySplash={replaySplash} isOverlay={isNavOverlay} theme={theme} />
-=======
       {showSplash && <SplashScreen key={splashKey}/>}
       <Nav replaySplash={replaySplash} isOverlay={isHome} theme={theme} />
->>>>>>> 7cb861251cf7e6c992449746c7b2e5ac6b2dceb4
       <main className="flex-1">
         <Outlet context={theme}></Outlet>
       </main>
