74a28c92309e4c16d5594f9467328a08bd63a60b
mann-prajapati05 <mannprajapati0284@gmail.com>
2026-05-14 11:34:10 +0530

    create blog content

--- frontend/src/routes/Blog_details.jsx
+++ frontend/src/routes/Blog_details.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import StretchHero from "../components/blog_components/StretchHero";
+import BlogContent from "../components/blog_components/BlogContent";
 
 function Blog_details() {
   return (
     <div>
       <StretchHero />
+      <BlogContent />
     </div>
   );
 }
