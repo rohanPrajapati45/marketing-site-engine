62a23f17f2ed09a3938ecac78f284006b1a3a698
Ayush Prajapati <ayushprajapati15806@gmail.com>
2026-05-19 09:25:21 +0530

    init admin panel

---
+++ Admin/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="bg-amber-400 text-3xl font-bold">My App</h1>
+    </div>
+  );
+}
+
+export default App;
