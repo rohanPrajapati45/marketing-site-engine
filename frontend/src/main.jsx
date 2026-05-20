b3417381a97969ee911fdc35b48028b25bd66aaa
Ayush Prajapati <ayushprajapati15806@gmail.com>
2026-05-20 18:34:23 +0530

    contact issue solved

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -29,7 +29,10 @@ const router = createBrowserRouter([
         path: "/:pageSlug/:blogSlug",
         element: <Blog_details />,
       },
-
+      {
+        path:"/contact",
+        element:<Contact />
+      },
       // GENERIC CMS PAGE
       {
         path: "/:slug",
